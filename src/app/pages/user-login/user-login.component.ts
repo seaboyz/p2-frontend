a52630039ff6bdb9e9de5f874fc81632a72c6b1a
seaboyz <seaboyz@hotmail.com>
2022-05-21 09:04:13 -0500

    update user-login

--- src/app/pages/user-login/user-login.component.ts
+++ src/app/pages/user-login/user-login.component.ts
@@ -5,7 +5,7 @@ import { AuthDto, AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.scss']
 })
 export class UserLoginComponent implements OnInit {
 
