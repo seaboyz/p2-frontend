82afd570c8805cb24965a206c2fe054de5001791
seaboyz <seaboyz@hotmail.com>
2022-05-21 07:08:54 -0500

    Add just button

--- src/app/component/add-to-cart-btn/add-to-cart-btn.component.ts
+++ src/app/component/add-to-cart-btn/add-to-cart-btn.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-to-cart-btn',
   templateUrl: './add-to-cart-btn.component.html',
-  styleUrls: ['./add-to-cart-btn.component.css']
+  styleUrls: ['./add-to-cart-btn.component.scss']
 })
-export class AddToCartBtnComponent implements OnInit {
+export class AddToCartBtnComponent implements OnInit
+{
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
   }
 
 }
