daa8ce228e039fea366da4a358bf8cb93427833d
seaboyz <seaboyz@hotmail.com>
2022-05-21 09:11:22 -0500

    update registration

--- src/app/pages/user-registration/user-registration.component.ts
+++ src/app/pages/user-registration/user-registration.component.ts
@@ -6,7 +6,7 @@ import { HttpHeaders, HttpRequest } from '@angular/common/http';
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
-  styleUrls: ['./user-registration.component.css']
+  styleUrls: ['./user-registration.component.scss']
 })
 export class UserRegistrationComponent implements OnInit {
 
