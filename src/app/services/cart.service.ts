93eb5a7cd4eb5ac65b8122c205ca3ddb0d9d2e16
seaboyz <seaboyz@hotmail.com>
2022-05-22 20:49:39 -0500

    Add CartItem interface

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Product } from "./product.service";
 
+interface CartItem extends Product
+{
+  quantity: number
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class CartService
 {
-  cartItems: { item: Product, quantity: number }[] = []
+  cartItems: CartItem[] = []
 
   constructor() { }
 
   addItem(itemToAdd: Product)
   {
-    const existItem = this.cartItems
-      .find(({ item }) => item.productId === itemToAdd.productId)
-
+    const existItem = this.cartItems.find(cartitem => cartitem.productId === itemToAdd.productId)
     if (existItem) {
       existItem.quantity++;
     } else {
-      this.cartItems.push({ item: itemToAdd, quantity: 1 })
+      this.cartItems.push({ ...itemToAdd, quantity: 1 })
     }
     console.log(this.cartItems)
   }
